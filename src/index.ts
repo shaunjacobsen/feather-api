72a168d1a874a2d399ec885c1db68f90d9ac46db
Shaun Jacobsen <shaun@sjacobsen.com>
2020-10-25 07:54:30 +0100

    Adds config files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello, world');
+});
+
+app.listen(3000);
